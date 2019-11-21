96edcdd37afebcfab3e9399dde53bdfdd8576db7
Colin Tinsman <tinsmancb@gmail.com>
2019-11-20 20:36:06 -0500

    chore: increment version number

--- setup.py
+++ setup.py
@@ -4,9 +4,10 @@ with open('README.md', 'r') as fh:
     long_description = fh.read()
 
 setup(name='multivector3d',
-      version='0.1',
+      version='0.1.1',
       description='Three Dimensional Multivector, based on numpy.',
       long_description=long_description,
+      long_description_content_type='text/markdown',
       author='Colin Tinsman',
       author_email='tinsmancb@gmail.com',
       url='https://github.com/tinsmancb/multivector3d',
